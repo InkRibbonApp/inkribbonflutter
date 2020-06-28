289d777d93ee4c09265a2d66723b4ab892577d7a
Rafael Delos Santos <rkdelossantos@gmail.com>
2020-06-28 22:41:25 +1000

    generated plugin

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,7 +8,6 @@ import 'dart:ui';
 import 'package:audioplayers/audioplayers_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
-import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -19,6 +18,5 @@ void registerPlugins(PluginRegistry registry) {
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
-  GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
   registry.registerMessageHandler();
 }
