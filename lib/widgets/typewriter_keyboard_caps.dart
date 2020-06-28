8442d81412586cf6a535d826445a3f5c85899e71
Rafael Delos Santos <rkdelossantos@gmail.com>
2020-06-28 20:12:21 +1000

    Fix typewriter caps layout

--- lib/widgets/typewriter_keyboard_caps.dart
+++ lib/widgets/typewriter_keyboard_caps.dart
@@ -10,112 +10,102 @@ class TypewriterKeyboardCaps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 240,
+      height: 220,
       child: Stack(
         fit: StackFit.expand,
         children: [
           Container(
             color: Colors.black87,
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildLetterKey('q-cap', 'Q'),
-                _buildLetterKey('w-cap', 'W'),
-                _buildLetterKey('e-cap', 'E'),
-                _buildLetterKey('r-cap', 'R'),
-                _buildLetterKey('t-cap', 'T'),
-                _buildLetterKey('y-cap', 'Y'),
-                _buildLetterKey('u-cap', 'U'),
-                _buildLetterKey('i-cap', 'I'),
-                _buildLetterKey('o-cap', 'O'),
-                _buildLetterKey('p-cap', 'P'),
-                _buildLetterKey('backspace', 'backspace'),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 60,
-            left: 30,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildLetterKey('a-cap', 'A'),
-                _buildLetterKey('s-cap', 'S'),
-                _buildLetterKey('d-cap', 'D'),
-                _buildLetterKey('f-cap', 'F'),
-                _buildLetterKey('g-cap', 'G'),
-                _buildLetterKey('h-cap', 'H'),
-                _buildLetterKey('j-cap', 'J'),
-                _buildLetterKey('k-cap', 'K'),
-                _buildLetterKey('l-cap', 'L'),
-                _buildEnterKey('enter', '\n'),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 120,
-            left: 15,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildLetterKey('shift', 'shift'),
-                _buildLetterKey('z-cap', 'Z'),
-                _buildLetterKey('x-cap', 'X'),
-                _buildLetterKey('c-cap', 'C'),
-                _buildLetterKey('v-cap', 'V'),
-                _buildLetterKey('b-cap', 'B'),
-                _buildLetterKey('n-cap', 'N'),
-                _buildLetterKey('m-cap', 'M'),
-                _buildLetterKey('comma', ','),
-                _buildLetterKey('dot', '.'),
-                _buildLetterKey('shift', 'shift'),
-              ],
-            ),
-          ),
-          Positioned(
-            top: 180,
-            left: 15,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildThickLetterKey('special_1', null),
-                _buildThickLetterKey('down_arrow', () {
-                  typewriterKeyboardController.setTypewriterState(
-                      TypewriterState(isOpen: false, type: typewriterKeyboardController.state.type));
-                }),
-                _buildSpaceLetterKey('space'),
-                _buildThickLetterKey('down_arrow', () {
-                  typewriterKeyboardController.setTypewriterState(
-                      TypewriterState(isOpen: false, type: typewriterKeyboardController.state.type));
-                }),
-              ],
-            ),
-          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildLetterKey('q-cap', 'Q'),
+                  _buildLetterKey('w-cap', 'W'),
+                  _buildLetterKey('e-cap', 'E'),
+                  _buildLetterKey('r-cap', 'R'),
+                  _buildLetterKey('t-cap', 'T'),
+                  _buildLetterKey('y-cap', 'Y'),
+                  _buildLetterKey('u-cap', 'U'),
+                  _buildLetterKey('i-cap', 'I'),
+                  _buildLetterKey('o-cap', 'O'),
+                  _buildLetterKey('p-cap', 'P'),
+                  _buildLetterKey('backspace', 'backspace'),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildLetterKey('a-cap', 'A'),
+                  _buildLetterKey('s-cap', 'S'),
+                  _buildLetterKey('d-cap', 'D'),
+                  _buildLetterKey('f-cap', 'F'),
+                  _buildLetterKey('g-cap', 'G'),
+                  _buildLetterKey('h-cap', 'H'),
+                  _buildLetterKey('j-cap', 'J'),
+                  _buildLetterKey('k-cap', 'K'),
+                  _buildLetterKey('l-cap', 'L'),
+                  _buildEnterKey('enter', '\n'),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildLetterKey('shift', 'shift'),
+                  _buildLetterKey('z-cap', 'Z'),
+                  _buildLetterKey('x-cap', 'X'),
+                  _buildLetterKey('c-cap', 'C'),
+                  _buildLetterKey('v-cap', 'V'),
+                  _buildLetterKey('b-cap', 'B'),
+                  _buildLetterKey('n-cap', 'N'),
+                  _buildLetterKey('m-cap', 'M'),
+                  _buildLetterKey('comma', ','),
+                  _buildLetterKey('dot', '.'),
+                  _buildLetterKey('shift', 'shift'),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildThickLetterKey('special_1', null),
+                  _buildThickLetterKey('down_arrow', () {
+                    typewriterKeyboardController.setTypewriterState(
+                        TypewriterState(isOpen: false, type: typewriterKeyboardController.state.type));
+                  }),
+                  _buildSpaceLetterKey('space'),
+                  _buildThickLetterKey('down_arrow', () {
+                    typewriterKeyboardController.setTypewriterState(
+                        TypewriterState(isOpen: false, type: typewriterKeyboardController.state.type));
+                  }),
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
   }
 
   Widget _buildLetterKey(String key, String text) {
-    return _buildKey(50, key, () {
+    return _buildKey(45, key, () {
       typewriterKeyboardController.addText(text);
     });
   }
 
   Widget _buildEnterKey(String key, String text) {
-    return _buildKey(80, key, () {
+    return _buildKey(70, key, () {
       typewriterKeyboardController.addText(text);
     });
   }
 
   Widget _buildThickLetterKey(String key, Function onTap) {
-    return _buildKey(80, key, onTap);
+    return _buildKey(70, key, onTap);
   }
 
   Widget _buildSpaceLetterKey(String key) {
@@ -127,7 +117,7 @@ class TypewriterKeyboardCaps extends StatelessWidget {
   Widget _buildKey(double width, String key, Function onTap) {
     return TypewriterKey(
       key: UniqueKey(),
-      height: 50,
+      height: 45,
       width: width,
       assetName: 'assets/keyboard/$key.png',
       onTap: onTap,
