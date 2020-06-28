270b4df6c5f524a029630588404cd3953148cd89
Rafael Delos Santos <rkdelossantos@gmail.com>
2020-06-28 23:20:21 +1000

    Refactor main file

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  var onBoardingState = await prefs.getBool("onBoardingState") ?? true;
-  if(onBoardingState) { await prefs.setBool('onBoardingState', false); }
+  final onBoardingState = prefs.getBool("onBoardingState") ?? true;
+  if (onBoardingState) {
+    await prefs.setBool('onBoardingState', false);
+  }
   runApp(TypewriterApp(onBoardingState));
 }
