bbbbefb8cf62dd7ffe231801dde57d67061e68c7
Rafael Delos Santos <rkdelossantos@gmail.com>
2020-06-28 10:57:01 +1000

    revert onboarding text to white text

--- lib/text_styles.dart
+++ lib/text_styles.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kOnboardingTextStyle = TextStyle(fontSize: 22, fontFamily: 'Underwood', color: Colors.black87);
-const kOnboardingButtonTextStyle = TextStyle(fontSize: 16, fontFamily: 'Underwood', color: Colors.black87);
+const kOnboardingTextStyle = TextStyle(fontSize: 22, fontFamily: 'Underwood');
+const kOnboardingButtonTextStyle = TextStyle(fontSize: 16, fontFamily: 'Underwood');
 const kLoginTextStyle = TextStyle(fontSize: 14, color: Colors.white);
