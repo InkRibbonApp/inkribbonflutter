2b3a552138319e75c7bcabee9f602763a5c87b72
Nick Fan <nfan@tyro.com>
2020-06-29 13:44:21 +1000

    PDF option for android and ios

--- lib/save_pdf.dart
+++ lib/save_pdf.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
@@ -9,12 +8,6 @@ Future<File> createPDF(String typeWriterText, String filename) async {
   final Document pdf = Document();
   pdf.addPage(Page(
       pageFormat: PdfPageFormat.a4,
-      theme: Theme.withFont(
-        base: Font.ttf(await rootBundle.load("assets/fonts/uwch.ttf")),
-        bold: Font.ttf(await rootBundle.load("assets/fonts/uwch.ttf")),
-        italic: Font.ttf(await rootBundle.load("assets/fonts/uwch.ttf")),
-        boldItalic: Font.ttf(await rootBundle.load("assets/fonts/uwch.ttf")),
-      ),
       build: (Context context) {
         return Center(
           child: Text(
@@ -23,7 +16,7 @@ Future<File> createPDF(String typeWriterText, String filename) async {
           ),
         ); // Center
       }));
-  var output = await getExternalStorageDirectory();
+  var output = await getTemporaryDirectory();
   final file = File('${output.path}/${filename}.pdf');
   file.writeAsBytesSync(pdf.save());
   return file;
